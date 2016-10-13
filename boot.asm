470e6c2ae6a6b752c18dc697db86234e49210950
Matthew Breeden <breeden.matt@gmail.com>
2016-10-13 01:11:10 -0700

    Add print_hexw macro for debugging

--- boot.asm
+++ boot.asm
@@ -1,7 +1,17 @@
 [bits 16]     ; We are in 16 bits real mode
 [org 0x7c00]  ; BIOS places us at this memory address
 
-jmp $
+jmp main
+
+%include 'print.asm'
+
+main:
+  mov si, start_boot_message
+  call print
+  cli
+  hlt
+
+start_boot_message: db 'Starting stage 0', 10, 13, 0
 
 ; BIOS looks for 0xaa55
 ; as last two bytes in a 512 byte segment
--- print.asm
+++ print.asm
@@ -15,3 +15,41 @@ print:
 .end:
   popa
   ret
+
+; %1 is any word sized register
+%macro print_hexw 1
+  ; uses ax, cx, and dx
+  pusha
+
+  mov dx, %1 ; store data from register param in dx
+
+  mov ah, 0x0e ; set BIOS write char flag
+  ; write out a preceeding 0x
+  mov al, 0x30
+  int 0x10
+  mov al, 0x78
+  int 0x10
+
+  mov cl, 4 ; We will be writing out 4 bytes
+  xor al, al ; Need to clear out al because upper nibble must be 0
+  ror dx, 12 ; get register in proper position for loop
+
+%%loop:
+  ror dx, 12
+  mov al, dl
+  shr al, 4 ; first nibble needs to be 0 for magic to work
+
+  ; magic that turns hex in lower nibble into
+  ; ascii printable version
+  cmp al, 0x0A
+  sbb al, 0x69
+  das
+
+  int 0x10
+
+  sub cl, 1
+  test cl, cl
+  jne %%loop
+
+  popa
+%endmacro
